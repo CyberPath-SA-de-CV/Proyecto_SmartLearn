3c12c289a94bd8099422906a985e982ec15a8c0c
CyberPath-SA-de-CV <cyberpathcontacto@gmail.com>
2025-04-25 02:02:43 -0600

    Primer avance Fase 3. Navegacion y ventanas iniciales

--- src/main/java/org/cyberpath/inicio/Inicio.java
+++ src/main/java/org/cyberpath/inicio/Inicio.java
@@ -3,6 +3,7 @@ package org.cyberpath.inicio;
 import org.cyberpath.vista.pantallas.PantallaInicio;
 
 public class Inicio {
+    //
     public static void main(String[] args) {
         new PantallaInicio().setVisible(true);
     }
